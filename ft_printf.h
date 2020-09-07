151324c06d470ad62c9fb64b30a231d283a95784
Darkfrok <gamerdarkfrok@gmail.com>
2020-09-07 11:13:29 -0700

    Deberes para casa 1/2 comienzo de estructuras

--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cquezada <cquezada@student.42.fr>          +#+  +:+       +#+        */
+/*   By: darkfrok <darkfrok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/27 12:54:51 by cquezada          #+#    #+#             */
-/*   Updated: 2020/09/04 13:26:01 by cquezada         ###   ########.fr       */
+/*   Updated: 2020/09/07 11:05:54 by darkfrok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 # define PRINTF_H
 # include "./Libft/libft.h"
 # include <stdarg.h>
+typedef	struct	s_flags
+{
+	int	width;
+	int	tiene_width;
+}				t_flags;
 
 int		ft_printf(const char *str, ...);
 void	ft_printstr(va_list args);
